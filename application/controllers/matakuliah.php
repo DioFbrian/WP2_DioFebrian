66ff9483abcf1e5c2662517c289aa9b8920d0862
Dio Febrian <diofebrian42@gmail.com>
2023-04-17 10:13:03 +0700

    P5 done

--- application/controllers/matakuliah.php
+++ application/controllers/matakuliah.php
@@ -1,18 +1,43 @@
 <?php
 class Matakuliah extends CI_Controller
 {
-    public $input;
     public function index()
     {
         $this->load->view('view-form-matakuliah');
     }
+
     public function cetak()
     {
-        $data = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks')
-        ];
-        $this->load->view('view-data-matakuliah', $data);
+        $this->load->library('form_validation');
+        $this->load->helper('input');
+        $this->form_validation->set_rules(
+            'kode',
+            'Kode Matakuliah',
+            'required|min_length[3]',
+            [
+                'required' => 'Kode Matakuliah Harus diisi',
+                'min_length' => 'Kode terlalu pendek'
+            ]
+        );
+        $this->form_validation->set_rules(
+            'nama',
+            'Nama Matakuliah',
+            'required|min_length[3]',
+            [
+                'required' => 'Nama Matakuliah Harus diisi',
+                'min_length' => 'Nama terlalu pendek'
+            ]
+        );
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('view-form-matakuliah');
+        } else {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
+            ];
+            $this->load->view('view-data-matakuliah', $data);
+        }
     }
 }
--- application/views/view-data-matakuliah.php
+++ application/views/view-data-matakuliah.php
@@ -37,8 +37,7 @@
             </tr>
             <tr>
                 <td colspan="3" align="center">
-                    <a href="<?= base_url('matakuliah');
-                                ?>">Kembali</a>
+                    <a href="<?= base_url('matakuliah'); ?>">Kembali</a>
                 </td>
             </tr>
         </table>
